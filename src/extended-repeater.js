56bd3ee67e3f18409a2998245926e19241bd93ed
Iryna Lytvynenko <frau4145@gmail.com>
2022-04-30 16:04:47 +0300

    feat: add solution for Extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let defaultSep = '+';
+  let defaultAdditionSep = "|";
+    
+  let addString = '';
+  let repeatedString = '';
+  if (options.hasOwnProperty('addition')) {
+    addString = options.addition;
+    if (options.hasOwnProperty('additionRepeatTimes')) {
+      for (let i = 1; i < options.additionRepeatTimes; i++) {
+        addString +=  options.hasOwnProperty('additionSeparator') ? 
+                      options.additionSeparator + options.addition :
+                      defaultAdditionSep + options.addition;
+      }
+    }
+  }
+  repeatedString = str + addString;
+  if (options.hasOwnProperty('repeatTimes')) { 
+    for (let i = 1; i < options.repeatTimes; i++) {
+      repeatedString += options.hasOwnProperty('separator') ? options.separator + str + addString : defaultSep + str + addString;
+    }
+  }
+  
+  return repeatedString;
 }
 
 module.exports = {
